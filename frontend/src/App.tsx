f4feb227a6605881a96dcfc4f9e89d2a848c0ddf
Sahana Deepak <sahanad149@gmail.com>
2024-07-28 23:32:13 -0700

    Complete code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
-import Header from "./components/Header"
-import { Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import Signup from "./pages/Signup"
-import Chat from "./pages/Chat"
-import NotFound from "./pages/NotFound"
-// import { useAuth } from "./context/AuthContext"
-
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Chat from "./pages/Chat";
+import NotFound from "./pages/NotFound";
+import { useAuth } from "./context/AuthContext";
+import Footer from "./components/footer/Footer";
 function App() {
-  // console.log(useAuth()?.isLoggedIn);
+  const auth = useAuth();
 
   return (
     <main>
@@ -17,11 +17,13 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/chat" element={<Chat />} />
+        {auth?.isLoggedIn && auth.user && (
+          <Route path="/chat" element={<Chat />} />
+        )}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,15 +1,55 @@
 import React from "react";
-import { AppBar, Toolbar } from "@mui/material";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Logo from "./shared/Logo";
+import { useAuth } from "../context/AuthContext";
+import NavigationLink from "./shared/NavigationLink";
 
 const Header = () => {
-    // return <div>Header</div>
-    return <AppBar sx = {{
-        bgcolor: "transparent",
-        position: "static",
-        boxShadow: "none"
-    }}>
-        <Toolbar sx={{ display: "flex" }}></Toolbar>
+  const auth = useAuth();
+  return (
+    <AppBar
+      sx={{ bgcolor: "transparent", position: "static", boxShadow: "none" }}
+    >
+      <Toolbar sx={{ display: "flex" }}>
+        <Logo />
+        <div>
+          {auth?.isLoggedIn ? (
+            <>
+              <NavigationLink
+                bg="#00fffc"
+                to="/chat"
+                text="Go To Chat"
+                textColor="black"
+              />
+              <NavigationLink
+                bg="#51538f"
+                textColor="white"
+                to="/"
+                text="logout"
+                onClick={auth.logout}
+              />
+            </>
+          ) : (
+            <>
+              <NavigationLink
+                bg="#00fffc"
+                to="/login"
+                text="Login"
+                textColor="black"
+              />
+              <NavigationLink
+                bg="#51538f"
+                textColor="white"
+                to="/signup"
+                text="Signup"
+              />
+            </>
+          )}
+        </div>
+      </Toolbar>
     </AppBar>
+  );
 };
 
 export default Header;
